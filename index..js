cb17c29aec80ae72b8ba031fb6ea07fd215ad4bd
AmanRohi <aman.rohi369@gmail.com>
2024-07-15 23:55:30 +0530

    hosting

--- index..js
+++ index..js
@@ -8,6 +8,25 @@ app.use(bodyParser.json());
 app.use(cors());
 const PORT = 8000;
 
+// Import the functions you need from the SDKs you need
+const { initializeApp } = require("firebase/app");
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDqLktLSfEF7GijU1FWPR9V3guf3FZ8RFE",
+  authDomain: "fruitybackend-29de7.firebaseapp.com",
+  projectId: "fruitybackend-29de7",
+  storageBucket: "fruitybackend-29de7.appspot.com",
+  messagingSenderId: "722380318409",
+  appId: "1:722380318409:web:994cea46666ff2a36ccf18"
+};
+
+// Initialize Firebase
+const a = initializeApp(firebaseConfig);
+
+
 const stripe = require("stripe")(process.env.STRIPE_KEY);
 app.post("/create-checkout-session", async (req, res) => {
   const { products } = req.body;
@@ -22,7 +41,6 @@ app.post("/create-checkout-session", async (req, res) => {
     },
     quantity: product.count,
   }));
-  console.log(lineItems);
 
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ["card"],
